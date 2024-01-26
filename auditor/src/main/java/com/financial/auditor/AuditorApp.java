81387f53982371e6b9946191e75e9611d2313532
Abdulaziz Alsaif (B522ALAB) <aslsaif@saib.com.sa>
2024-01-26 14:13:49 +0300

    init commit

---
+++ auditor/src/main/java/com/financial/auditor/AuditorApp.java
@@ -0,0 +1,10 @@
+package com.financial.auditor;
+
+public class AuditorApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("AuditorApp");
+    }
+}
---
+++ traders/src/main/java/com/financial/traders/TradersApp.java
@@ -0,0 +1,9 @@
+package com.financial.traders;
+
+public class TradersApp {
+
+
+    public static void main(String[] args) {
+        System.out.println("TradersApp");
+    }
+}
