d877506b062b04dba4c14fbc2827417244048fb0
Abdulaziz Alsaif (B522ALAB) <aslsaif@saib.com.sa>
2024-01-26 19:06:05 +0300

    quote-generator/generate message as json

--- quote-generator/src/main/java/com/financial/quote/generator/actors/QuoteGeneratorProducer.java
+++ quote-generator/src/main/java/com/financial/quote/generator/actors/QuoteGeneratorProducer.java
@@ -5,6 +5,8 @@ import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.kafka.ProducerSettings;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.financial.quote.generator.entity.Quote;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,12 +14,15 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 public class QuoteGeneratorProducer extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    private final ObjectWriter objectMapper;
     private final ProducerSettings<String, String> producerSettings;
     private Producer<String,String> kafkaProducer;
     public QuoteGeneratorProducer() {
         this.producerSettings = ProducerSettings.create(getContext().getSystem(), new StringSerializer(), new StringSerializer())
             .withBootstrapServers("localhost:9092");
         this.kafkaProducer = producerSettings.createKafkaProducer();
+        this.objectMapper=new ObjectMapper().writer().withDefaultPrettyPrinter();
     }
 
     @Override
@@ -25,9 +30,9 @@ public class QuoteGeneratorProducer extends AbstractActor {
         return receiveBuilder()
             .match(Quote.class, quote -> {
                 String kakfaTopic="financial-quotes";
-                ProducerRecord<String, String> record = new ProducerRecord<>(kakfaTopic, quote.getCompanyName(), String.valueOf(quote.getPrice()));
+                ProducerRecord<String, String> record = new ProducerRecord<>(kakfaTopic, quote.getCompanyName(), objectMapper.writeValueAsString(quote));
                 kafkaProducer.send(record);
-                log.info("Producing to kafka topic {} , with values {}",kakfaTopic,quote);
+                log.info("Producing to kafka topic {} , with values {}",record);
             })
             .build();
     }
